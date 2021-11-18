fc9c7d5591da80557252a1dd63fb9ee1b7b87af6
Oprea Stefan <opreastef97@gmail.com>
2021-11-18 22:28:42 +0200

    clean

--- src/js/controller.js
+++ src/js/controller.js
@@ -124,10 +124,6 @@ const controlAddRecipe = async function (newRecipe) {
     }
 };
 
-const newFeature = function() {
-    console.log('Welcome to the application');
-}
-
 const init = function () {
     bookmarksView.addHandlerRender(controlBookmarks);
     recipeView.addHandlerRender(controlRecipes);
@@ -136,6 +132,5 @@ const init = function () {
     searchView.addHandlerSearch(controlSearchResults);
     paginationView.addHandlerClick(controlPagination);
     addRecipeView.addHandlerUpload(controlAddRecipe);
-    newFeature();
 };
 init();
