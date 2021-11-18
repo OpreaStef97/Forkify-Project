28580a8cb9858e296e029d6e81efbeea0d3b6c09
Oprea Stefan <opreastef97@gmail.com>
2021-11-18 21:41:42 +0200

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -124,6 +124,10 @@ const controlAddRecipe = async function (newRecipe) {
     }
 };
 
+const newFeature = function() {
+    console.log('Welcome to the application');
+}
+
 const init = function () {
     bookmarksView.addHandlerRender(controlBookmarks);
     recipeView.addHandlerRender(controlRecipes);
@@ -132,6 +136,6 @@ const init = function () {
     searchView.addHandlerSearch(controlSearchResults);
     paginationView.addHandlerClick(controlPagination);
     addRecipeView.addHandlerUpload(controlAddRecipe);
-    console.log('Welcome');
+    newFeature();
 };
 init();
